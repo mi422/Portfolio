26ade493118050d25dcabaca3bfce2d4720a6fde
oumaima <missaomayma@gmail.com>
2021-12-17 20:30:04 +0100

    Deploy portoflio

--- src/pages/Skills/Skills-data.js
+++ src/pages/Skills/Skills-data.js
@@ -1,10 +1,6 @@
 import L_HTML5 from "../../assets/img/skills/html-5.svg";
 import L_CSS3 from "../../assets/img/skills/css3.svg";
 import L_BOOTSTRAP from "../../assets/img/skills/bootstrap-4.svg";
-import L_REACT from "../../assets/img/skills/react.svg";
-import L_REACT_ROUTER from "../../assets/img/skills/react-router.svg";
-import L_REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
-import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
 import L_GIT from "../../assets/img/skills/git-icon.svg";
 import L_GITHUB_PAGES from "../../assets/img/skills/github.svg";
 import L_JAVASCRIPT from "../../assets/img/skills/javascript.svg";
@@ -37,30 +33,6 @@ export const skills={
             imgAltText: "Bootstrap",
             imgSrc: L_BOOTSTRAP,
             skillName: "Bootstrap",
-          },
-          {
-            link: "https://reactjs.org/",
-            imgAltText: "React JS",
-            imgSrc: L_REACT,
-            skillName: "React JS",
-          },
-          {
-            link: "https://reacttraining.com/react-router/",
-            imgAltText: "React Router",
-            imgSrc: L_REACT_ROUTER,
-            skillName: "React Router",
-          },
-          {
-            link: "https://react-bootstrap.github.io/",
-            imgAltText: "React Bootstrap",
-            imgSrc: L_REACT_BOOTSTRAP,
-            skillName: "React Bootstrap",
-          },
-          {
-            link: "https://material-ui.com/",
-            imgAltText: "Material-UI",
-            imgSrc: L_MATERIALUI,
-            skillName: "Material-UI",
           },
     ],
       hostingPlatforms: [
